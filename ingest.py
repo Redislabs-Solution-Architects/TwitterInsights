1b8a82acccd3ee1438a98b0b95ea6fd76db4146f
danlowen <dan.lowen@redislabs.com>
2019-11-30 01:01:03 -0500

    More cleanup

--- ingest.py
+++ ingest.py
@@ -4,25 +4,22 @@ from pubnub.pnconfiguration import PNConfiguration
 from pubnub.pubnub import PubNub, SubscribeListener
 
 from redis import Redis
-import redis
+import redis, socket, os
 
 import argparse
 
 # process command line arguments
 parser = argparse.ArgumentParser(description="Program to ingest twitter feed into a Redis Stream")
 parser.add_argument("-s", "--server", help="Redis endpoint hostname",required=True)
-parser.add_argument("-p", "--port", help="Redis endpoint port",required=True)
+parser.add_argument("-p", "--port", type=int, help="Redis endpoint port",required=True)
 args = parser.parse_args()
 
-# --host and --port are required
-if not args.server or not args.port:
-    print("--host and --port are required parameters")
-    exit()
-
-# Connect to Redis
+# Construct a Redis client
+host = args.server
+port = args.port
 redis_client = redis.Redis(
-    host=args.server,
-    port=args.port,
+    host=host,
+    port=port,
     decode_responses=True )
 
 def flatten_list(l, parent_key='', sep=':'):
@@ -44,7 +41,7 @@ def flatten_dict(d, parent_key='', sep=':'):
     items = []
     for k, v in d.items():
         new_key = parent_key + sep + k.strip() if parent_key else k.strip()
-        if isinstance(v, dict) : # and not isinstance(v, (str, bytes, bytearray)):
+        if isinstance(v, dict) :
             items.extend(flatten_dict(v, new_key, sep=sep).items())
         elif isinstance(v,list) :
             items.extend(flatten_list(v, new_key, sep=sep).items())
@@ -52,29 +49,38 @@ def flatten_dict(d, parent_key='', sep=':'):
             items.append((new_key, str(v)))
     return dict(items)
 
-
+# Construct PubNub client
 pnconfig = PNConfiguration()
 pnconfig.subscribe_key = "sub-c-78806dd4-42a6-11e4-aed8-02ee2ddab7fe"
 pnconfig.ssl = False
-
 pubnub = PubNub(pnconfig)
 
+# Subscribe to Twitter channel
 my_listener = SubscribeListener()
 pubnub.add_listener(my_listener)
-
 pubnub.subscribe().channels('pubnub-twitter').execute()
 my_listener.wait_for_connect()
-print('connected')
 
+# Process published Twitter messages
 while True:
+    # Get next published Tweet
     result = my_listener.wait_for_message_on('pubnub-twitter')
-#    print('result.message = ' + str(result.message))
+
+    # Flatten Tweet's nested content
     flattened = flatten_dict(result.message)
-#    print('flattened = ' + str(flattened))
-    redis_client.xadd('twitter', flattened, maxlen=100000)
+    
+    # Add flattened Tweet to a Redis Stream
+    try :
+        redis_client.xadd('twitter', flattened, maxlen=100000)
+    except redis.exceptions.ConnectionError:
+        print("Can't connect to Redis DB at: " + host + ":" + str(port))
+        os._exit(1)
+    except :
+        print("XADD failed")
+        os._exit(2)
 
 pubnub.unsubscribe().channels('pubnub-twitter').execute()
 my_listener.wait_for_disconnect()
 
-print('unsubscribed')
+os._exit(0)
 
